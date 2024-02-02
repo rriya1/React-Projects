a876c9b2b201f777c44bca038c1dcce630ff046b
rriya1 <rriya1_be20@thapar.edu>
2024-02-02 14:42:21 +0530

    adding more functionality

--- To_learn_concept/learn_basics/src/index.js
+++ To_learn_concept/learn_basics/src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from "react-router-dom";
+import Form from "./components/Form";
+import Learn from "./components/Learn";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route path="/components/Learn" element={<Learn />} />
+			<Route path="/components/Form" element={<Form />} />
+		</Route>
+	)
 );
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
