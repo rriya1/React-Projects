aa868bd632c18249cb54c6fbe64596418483527e
rriya1 <rriya1_be20@thapar.edu>
2024-02-09 16:10:15 +0530

    API call
    implemented logic to call the TMDB api with added security to hide the API in the url.

--- moviex/src/App.jsx
+++ moviex/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
 
 function App() {
-
-  return (
-    <>
-      <h1> my project start</h1>
-    </>
-  )
+	return (
+		<>
+			<h1> my project start</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
