5fccaabd23cab4ec5b5f1ecaacc7a483c0a7539f
rriya1 <rriya1_be20@thapar.edu>
2024-02-09 15:11:52 +0530

    moviex project starting
    start of a fully functional project with react, scss,redux toolkit, routers etc.
    edited the initial file

---
+++ moviex/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <h1> my project start</h1>
+    </>
+  )
+}
+
+export default App
